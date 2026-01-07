9911151a310d38118cba936267605aea8fa51a03
Usama-Dev <usamahassan311@gmail.com>
2026-01-07 16:53:58 +0500

    all_done

--- app/database/configuration.py
+++ app/database/configuration.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 MONGO_URL = "mongodb+srv://usamahassan311_db_user:sORJeo98Eu4buhXX@cluster0.jhhjdft.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_URL)
-db = client.bank_transaction_db
+db = client.finance_db
 
-users_collection = db.bank_customer
-transactions_collection = db.customer_transactions
+users_collection = db.users
+transactions_collection = db.transactions
